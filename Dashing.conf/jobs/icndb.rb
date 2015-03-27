a8731bc0635e1f0928caafd34ffae34e20600b5b
Usman Iqbal <usmanatron@hotmail.com>
2015-03-27 22:42:17 +0000

    Add scheduled job to show a new Chuck Norris fact every minute on the motd widget

---
+++ Dashing.conf/jobs/icndb.rb
@@ -0,0 +1,19 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
+server = "http://api.icndb.com"
+
+SCHEDULER.every '24h', :first_in => 0 do |job|
+  url = URI.parse("#{server}/jokes/random?limitTo=[nerdy]")
+  http = Net::HTTP.new(url.host, url.port)
+  http.use_ssl = (url.scheme == 'https')
+  http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+  response = http.request(Net::HTTP::Get.new(url.request_uri))
+
+  # Convert to JSON and retrieve joke
+  j = JSON[response.body]
+  joke = j['value']['joke']
+
+  send_event('motd', { message: "#{joke}" , fromScheduledJob: true })
+end
